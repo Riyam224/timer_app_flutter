2625a2bf95c35cecdf4474c520abb913a5e98548
Riyam224 <cs50riyam211#gmail.com>
2024-03-24 00:46:19 +0300

    make list of sounds and call sound as var in widget and call it again in when call the the widget int the main widget

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,53 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  // todo list of sounds
+
+  List<String> sounds = [
+    'sounds/note1.wav',
+    'sounds/note2.wav',
+    'sounds/note3.wav',
+    'sounds/note4.wav',
+    'sounds/note5.wav',
+    'sounds/note6.wav',
+    'sounds/note7.wav',
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Tune App',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          'Tune App',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
           ),
-          centerTitle: true,
         ),
-        body: Column(
-          children: [
-            ColorWidget(
-              color: Color.fromARGB(255, 222, 133, 38),
-              sound: 'sounds/note1.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 237, 216, 57),
-              sound: 'sounds/note2.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 57, 108, 14),
-              sound: 'sounds/note3.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 38, 182, 222),
-              sound: 'sounds/note4.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 23, 36, 168),
-              sound: 'sounds/note5.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 32, 3, 3),
-              sound: 'sounds/note6.wav',
-            ),
-            ColorWidget(
-              color: Color.fromARGB(255, 48, 12, 87),
-              sound: 'sounds/note7.wav',
-            ),
-          ],
-        ));
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          ColorWidget(
+            color: Colors.black12,
+            sound: sounds[1],
+          ),
+          ColorWidget(
+            color: Colors.black12,
+            sound: sounds[1],
+          ),
+          ColorWidget(
+            color: const Color.fromARGB(31, 37, 9, 9),
+            sound: sounds[2],
+          ),
+          ColorWidget(
+            color: Color.fromARGB(31, 180, 251, 25),
+            sound: sounds[4],
+          ),
+        ],
+      ),
+    );
   }
 }
 // todo category widget
